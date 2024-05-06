3c16a466d5af4a8addc6db5a87035a04649a4ecc
swetha <swethareddycv098@gmail.com>
2024-05-06 15:18:28 +0200

    add new exercises

--- labs/week1.py
+++ labs/week1.py
@@ -1,3 +1,20 @@
+# Given String
+s = "python"
+print(s[0:1])
+print(s[0:4])
+print(s[1:4])
+print(s[::-1])
+
+# output will be [b,c] -1 is not inclue last element
+# list = ['a', 'b', 'c', 'd']
+#  print(list[1:-1])
+
+# Given the Nested List
+i = [3,7,[1,4,"hello"]]
+i[2][2] = "goodbye"
+print(i)
+
+
 # using keys and indexing,grab the "hello" from the following dictionaries:
 
 d1 = {'simple_key': 'hello'}
@@ -14,3 +31,29 @@ d3 = {'k1':
       ]
      }
 print(d3['k1'][0]['nest_key'][1][0])
+
+# Use a set of find the unique values of the list below:
+
+mylist = [1,1,1,1,2,2,2,2,3,3,3]
+a = set(mylist)
+print(a)
+
+
+#you are giving two variable:
+age = 4
+name = "sammy"
+
+print(f"Hello my dog name is {name} and he is {age} years old")
+print("Hello my dog name is {} and he is {} years old".format(name,age))
+
+
+student = {"Ram":{"name":"manga", "age":20, "course":"py"},
+           "mohan":{"roll_no":20, "age":22, "course":"java"}}
+           
+print(student["mohan"])
+print(student["Ram"]["age"])
+student["mohan"]["phone"]=15356353
+print(student["mohan"])
+del student["mohan"]["phone"]
+print(student["mohan"])
+
